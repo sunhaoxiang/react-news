b854fda428d49f4a0e951a7d86893b5044008709
孙昊翔 <673686754@qq.com>
2017-04-04 16:19:59 +0800

    pc_header

--- src/js/root.js
+++ src/js/root.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {Button} from 'antd';
+import PCIndex from './components/pc_index';
 import 'antd/dist/antd.css';
-
 export default class Root extends React.Component {
 	render() {
 		return (
 			<div>
-				<Button type="primary">Primary</Button>
-				<Button>Default</Button>
-				<Button type="ghost">Ghost</Button>
-				<Button type="dashed">Dashed</Button>
+				<PCIndex/>
 			</div>
 		);
 	};
